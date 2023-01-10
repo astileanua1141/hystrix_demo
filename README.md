7605a41c221a4d8bd10e3bc181b1000e33a1aae1
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-11 01:42:20 +0200

    Circuit Breaker Setup Test

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 [![Java CI - Run Junit Tests](https://github.com/astileanua1141/hystrix_demo/actions/workflows/myTest.yaml/badge.svg)](https://github.com/astileanua1141/hystrix_demo/actions/workflows/myTest.yaml)
 
 # Hystrix Demo
-Just having a look at Hystrix 
+### Just having a look at Hystrix 
 
+In summary Hystrix is designed to:
+- Provide protection and control over failures and latency from services typically accessed over the network
+- Stop cascading of failures resulting from some of the services being down
+- Fail fast and rapidly recover
+- Degrade gracefully where possible
+- Real time monitoring and alerting of command center on failures
--- src/test/java/org/astileanua1141/hystrix/HystrixTimeoutManualTest.java
+++ src/test/java/org/astileanua1141/hystrix/HystrixTimeoutManualTest.java
@@ -13,6 +13,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HystrixTimeoutManualTest {
 
+    public String invokeRemoteService(HystrixCommand.Setter config, int timeout) throws InterruptedException {
+        String response = null;
+
+        try {
+            response = new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(timeout)).execute();
+        } catch (HystrixRuntimeException ex) {
+            System.out.println("ex = " + ex);
+        }
+
+        return response;
+    }
+
     @Test
     public void givenInputBobAndDefaultSettings_whenCommandExecuted_thenReturnHelloBob() {
         assertThat(new CommandHelloWorld("Bobby").execute(), equalTo("Hello Bobby"));
@@ -78,5 +90,39 @@ public class HystrixTimeoutManualTest {
         assertThat(new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(500)).execute(),
                 equalTo("Success!"));
     }
+
+    @Test
+    public void givenCircuitBreakerSetup_whenRemoteSvcCmdExecuted_thenReturnSuccess() throws InterruptedException {
+        HystrixCommand.Setter config = HystrixCommand
+                .Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey("RemoteServiceGroupCircuitBreaker"));
+
+        HystrixCommandProperties.Setter properties = HystrixCommandProperties.Setter();
+        properties.withExecutionTimeoutInMilliseconds(1000);
+        // Circuit Breaker Sleep Window - the time interval after which the request to the remote service will be resumed
+        properties.withCircuitBreakerSleepWindowInMilliseconds(4000);
+        properties.withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.THREAD);
+        properties.withCircuitBreakerEnabled(true);
+        //Circuit Breaker Volume Threshold - minimum number of requests needed before the failure rate will be considered
+        properties.withCircuitBreakerRequestVolumeThreshold(1);
+
+        config.andCommandPropertiesDefaults(properties);
+        config.andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
+                .withMaxQueueSize(1)
+                .withCoreSize(1)
+                .withQueueSizeRejectionThreshold(1));
+
+        assertThat(this.invokeRemoteService(config, 10_000), equalTo(null));
+        assertThat(this.invokeRemoteService(config, 10_000), equalTo(null));
+        assertThat(this.invokeRemoteService(config, 10_000), equalTo(null));
+
+        Thread.sleep(5000);
+
+        assertThat(new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(500)).execute(), equalTo("Success!"));
+        assertThat(new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(500)).execute(), equalTo("Success!"));
+        assertThat(new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(500)).execute(), equalTo("Success!"));
+
+    }
+
 
 }
