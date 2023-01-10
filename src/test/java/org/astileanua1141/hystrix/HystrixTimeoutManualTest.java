4ff5d5b78494e33a2034d3957234cea2d5746fc0
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-10 22:56:27 +0200

    Short Circuiting via (remote service) Timeout on HystrixCommand

--- src/test/java/org/astileanua1141/hystrix/HystrixTimeoutManualTest.java
+++ src/test/java/org/astileanua1141/hystrix/HystrixTimeoutManualTest.java
@@ -2,6 +2,8 @@ package org.astileanua1141.hystrix;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -25,5 +27,35 @@ public class HystrixTimeoutManualTest {
 
         assertThat(new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(100)).execute(),
                 equalTo("Success!"));
+    }
+
+    @Test
+    public void
+    givenSvcTimeoutOf5000AndExecTimeoutOf10000_whenRemoteSvcExecuted_thenReturnSuccess()
+            throws InterruptedException {
+        HystrixCommand.Setter config = HystrixCommand
+                .Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey("RemoteServiceGroup2"));
+
+        HystrixCommandProperties.Setter commandProperties = HystrixCommandProperties.Setter();
+        commandProperties.withExecutionTimeoutInMilliseconds(10_000);
+        config.andCommandPropertiesDefaults(commandProperties);
+
+        assertThat(new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(500)).execute(),
+                equalTo("Success"));
+    }
+
+    @Test(expected = HystrixRuntimeException.class)
+    public void givenSvcTimeoutOf15000AndExecTimeoutOf5000_whenRemoteEvcExecuted_thenExpectHre() throws InterruptedException {
+        HystrixCommand.Setter config = HystrixCommand
+                .Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey("RemoteServiceGroup3"));
+
+        HystrixCommandProperties.Setter commandProperties = HystrixCommandProperties.Setter();
+        commandProperties.withExecutionTimeoutInMilliseconds(5_000);
+        config.andCommandPropertiesDefaults(commandProperties);
+
+        new RemoteServiceTestCommand(config, new RemoteServiceTestSimulator(15_000)).execute();
+
     }
 }
